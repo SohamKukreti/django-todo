32325a961b96f2fba39947154eabf7b74ea0fcb4
Soham Kukreti <kukretisoham@gmail.com>
2024-06-17 00:41:49 +0530

    Work on logic to add tasks

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.index, name = "index"),
     path("add/", views.add, name = "add"),
     path("<int:pk>/delete/", views.delete, name = "delete"),
+    path("get_task_info/", views.info, name = "get_task_info")
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models.query import QuerySet
+from django.urls import reverse
+from django.utils import timezone
 from .models import Task
 # Create your views here.
 def index(request):
@@ -8,7 +10,12 @@ def index(request):
     return render(request, "todo/index.html", {"tasks" : t})
 
 def add(request):
-    return HttpResponse("You are at add page")
+    task = Task.objects.create(task_name = request.POST["task_name"], created_at = timezone.now())
+    task.save()
+    return HttpResponseRedirect(reverse("todo:index"))
 
 def delete(request, pk):
     return HttpResponse("You are deleting task %s" % pk)
+
+def info(request):
+    return render(request, "todo/add.html")
